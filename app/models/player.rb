b90c72194e0fca322c7b1488201bd0f5040e083f
Aderex0 <54026981+Aderex0@users.noreply.github.com>
2019-12-10 16:20:02 +0000

    Update player.rb

--- app/models/player.rb
+++ app/models/player.rb
@@ -97,8 +97,8 @@ class Player < ApplicationRecord
         
         response = Unirest.get "https://api-nba-v1.p.rapidapi.com/players/league/standard",
             headers: {
-                "X-RapidAPI-Host" => "api-nba-v1.p.rapidapi.com",
-                "X-RapidAPI-Key" => "643c94eea2msh53636626512870fp1c3810jsnfef21a3c2700"
+                "X-RapidAPI-Host" => "key",
+                "X-RapidAPI-Key" => "key"
         }
     end
 
